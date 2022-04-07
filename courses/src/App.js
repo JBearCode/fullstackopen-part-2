f6a729d7b94767c962e0004376ea0cd8c0f32c31
Joel Moran <joelbmoran@gmail.com>
2022-04-07 20:44:12 +0700

    Refactor into additional child component Course for Exercise 2.1

--- courses/src/App.js
+++ courses/src/App.js
@@ -1,29 +1,40 @@
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack Application Development',
     parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10,
+        id: 1,
       }, 
       {
         name: 'Using props to pass data',
         exercises: 7,
+        id: 2,
       }, 
       {
         name: 'State of a component',
         exercises: 14,
+        id: 3,
       }
     ] 
   }
   
   return (
-    <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
-    </div>
-  );
+  <Course course={course}/>
+  )
+}
+
+const Course = (props) => {
+  console.log(props.course);
+  return (
+  <div>
+  <Header course={props.course}/>
+  <Content course={props.course}/>
+  <Total course={props.course}/>
+  </div>
+  )
 }
 
 const Header = (props) => {
@@ -33,7 +44,6 @@ const Header = (props) => {
   </>
   )
 }
-
 
 const Content = (props) => {
   return (
@@ -54,7 +64,6 @@ const Part = (props) => {
     </>
   )
 }
-
 
 const Total = (props) => {
   return (
