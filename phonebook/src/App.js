723b415800ecee568b0229b8c9f32a2cc4c098f3
Joel Moran <joelbmoran@gmail.com>
2022-04-08 09:52:10 +0700

    Add functionality to receive and list phone numbers along with names (Exercise 2.8)

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -5,45 +5,52 @@ const App = () => {
     { name: 'Arto Hellas', id: 0 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
 
   const checkForDuplicate = (name) => persons.map(ob => ob.name).includes(name)
 
-  const addNumber = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
     const numberObject = {
       name: newName,
+      number: newNumber,
       id: persons.length,
     }
     if (checkForDuplicate(numberObject.name)) {
       alert(`${numberObject.name} is already in the phonebook!`)
       setNewName('');
+      setNewNumber('');
     } else {
       setPersons(persons.concat(numberObject))
       setNewName('');
+      setNewNumber('');
     }
   }
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value);
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value);
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNumber}>
-        <div>
-          name: <input value={newName}
-                       onChange={handleInputChange}
-                />
-        </div>
+      <form onSubmit={addPerson}>
+        <div>name: <input type="text" value={newName} onChange={handleNameChange}/></div>
+        <div>number: <input type="number" value={newNumber} onChange={handleNumberChange}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person, i) => 
-        <p key={person.id}>{person.name}</p>
+        <p key={person.id}>{person.name} {person.number}</p>
       )}
     </div>
   )
