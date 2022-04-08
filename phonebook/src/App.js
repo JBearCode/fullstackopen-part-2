207e0cc56baffc2dc01ea3f7b9faf24e783d9e91
Joel Moran <joelbmoran@gmail.com>
2022-04-08 09:02:27 +0700

    Create controlled component and event handlers to add submitted names to DOM (Exercise 2.6)

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,8 +1,46 @@
-function App() {
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', id: 0 }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    const numberObject = {
+      name: newName,
+      id: persons.length,
+    }
+    setPersons(persons.concat(numberObject))
+    setNewName('');
+  }
+
+  const handleInputChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value);
+  }
+
   return (
-    <div className="App">
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName}
+                       onChange={handleInputChange}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => 
+        <p key={person.id}>{person.name}</p>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
