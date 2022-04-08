3976047712ee8a08ac3f37b9ff1df0b748b337e7
Joel Moran <joelbmoran@gmail.com>
2022-04-08 10:46:14 +0700

    Add case-insensitive search field to filter names (Exercise 2.9)

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,11 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', id: 0 }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [filterBy, setFilterBy] = useState('')
+  const [personsToDisplay, setPersonsToDisplay] = useState(persons)
 
   const checkForDuplicate = (name) => persons.map(ob => ob.name).includes(name)
 
@@ -15,13 +19,14 @@ const App = () => {
     const numberObject = {
       name: newName,
       number: newNumber,
-      id: persons.length,
+      id: persons.length + 1,
     }
     if (checkForDuplicate(numberObject.name)) {
       alert(`${numberObject.name} is already in the phonebook!`)
       setNewName('');
       setNewNumber('');
     } else {
+      setPersonsToDisplay(persons.concat(numberObject));
       setPersons(persons.concat(numberObject))
       setNewName('');
       setNewNumber('');
@@ -29,18 +34,31 @@ const App = () => {
   }
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
     setNewName(event.target.value);
   }
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value)
     setNewNumber(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilterBy(event.target.value);
+    console.log(event.target.value);
+    if (!event.target.value) {
+      setPersonsToDisplay(persons)
+    } else {
+    setPersonsToDisplay(persons.filter(person =>
+      person.name.toLowerCase().includes(filterBy.toLowerCase()) === true
+    ));}
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>filter contacts: <input 
+          type="text" value={filterBy} onChange={handleFilterChange}
+        /></div>
+      <h2>Add Contact</h2>
       <form onSubmit={addPerson}>
         <div>name: <input type="text" value={newName} onChange={handleNameChange}/></div>
         <div>number: <input type="number" value={newNumber} onChange={handleNumberChange}/></div>
@@ -49,7 +67,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person, i) => 
+      {personsToDisplay.map((person) => 
         <p key={person.id}>{person.name} {person.number}</p>
       )}
     </div>
