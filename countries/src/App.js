319436a50d9f5025f56b516ae3a5079d18eb2142
Joel Moran <joelbmoran@gmail.com>
2022-04-11 12:30:06 +0700

    Completed core functionality, working to find bug where state is only set from API sometimes

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,23 +1,21 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-function App() {
-  const [countries, setCountries] = useState([]);
+const App = () => {
+  const [countries, setCountries] = useState([])
   const [names, setNames] = useState([])
   const [filter, setFilter] = useState('');
   const [filteredCountries, setFiltered] = useState([])
+  let responseData;
 
   const handleFilterChange = (value) => {
     setFilter(value);
-    console.log(value);
     if (!value) {
       setFiltered(names);
     } else {
-      let matching = names.filter(country =>
+      setFiltered(names.filter(country =>
         country.toLowerCase().includes(filter.toLowerCase())
-      )
-      console.log(matching);
-      setFiltered(matching);
+      ))
     }
   }
 
@@ -27,9 +25,10 @@ function App() {
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
         console.log('promise fulfilled')
-        console.log(response.data)
-        setCountries(response.data)
-        console.log(countries);
+        responseData = response.data
+        console.log('response.data', responseData)
+        setCountries(responseData)
+        console.log("countries array", countries);
         populateCountryNames();
       })
   }, [])
@@ -40,14 +39,13 @@ function App() {
     )
     setNames(countryNames)
     setFiltered(countryNames)
-    console.log(countryNames)
   }
 
   return (
     <div className="App">
       <h1>Know Your Nations</h1>
       <Search filterBy={filter} onChange={handleFilterChange} />
-      <Information filteredCountries={filteredCountries} />
+      <Information filteredCountries={filteredCountries} countries={countries} />
     </div>
   );
 }
@@ -62,9 +60,9 @@ const Search = ({filter, onChange}) => {
   )
 }
 
-const Information = ({filteredCountries}) => {
+const Information = ({filteredCountries, countries}) => {
   let length = filteredCountries.length;
-  console.log(length);
+  console.log("filtered countries arriving in Information component", length);
 
   if (length === 0) {
     return (
@@ -73,9 +71,31 @@ const Information = ({filteredCountries}) => {
       </div>
     )
   } else if (length === 1) {
+    console.log("full info on one country", countries)
+    let countryInfo = countries.filter(country =>
+     country.name.common === filteredCountries[0]
+     )
+    console.log("country info", countryInfo);
+
+
     return (
-      <div>
-        <p>We've got lots of info on {filteredCountries[0]}!</p>
+    <div>
+      <h2>{filteredCountries[0]} {countryInfo[0].flag}</h2>
+      <p>We've got lots of info on {filteredCountries[0]}!</p>
+      {/*
+      */}
+      <img src={countryInfo[0].flags.png} alt="flag image" width="250"/>
+      <p>Official Name: {countryInfo[0].name.official}</p>
+      <p>Region: {countryInfo[0].subregion}</p>
+      <p>Population: {countryInfo[0].population}</p>
+      <p>Coat of Arms:</p>
+      <img src={countryInfo[0].coatOfArms.png} alt="coat of arms" width="250"/>
+      <p>Capital City: {countryInfo[0].capital[0]}</p>
+      <p>{filteredCountries[0]} is {countryInfo[0].independent ? "independent" : "not independent"
+      } and {countryInfo[0].landlocked ? "is landlocked" : "is not landlocked"
+      }. It {countryInfo[0].unMember ? "is" : "is not"} a member of the United Nations.
+      
+      </p>
       </div>
       )
   } else if (length > 10) {
@@ -87,8 +107,8 @@ const Information = ({filteredCountries}) => {
   } else
   return (
       <div>
-        {filteredCountries.map(country =>
-          <p>{country}</p>  
+        {filteredCountries.map((country, i) =>
+          <p key={i}>{country}</p>  
         )}
       </div>
     )
