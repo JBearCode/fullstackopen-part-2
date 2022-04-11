c6d3d73509a79dba26cbf97953391cb6182b0695
Joel Moran <joelbmoran@gmail.com>
2022-04-11 14:05:20 +0700

    Fix state rendering error by adding 2nd useEffect function that triggers on state change of variable 'countries'

--- countries/src/App.js
+++ countries/src/App.js
@@ -6,7 +6,6 @@ const App = () => {
   const [names, setNames] = useState([])
   const [filter, setFilter] = useState('');
   const [filteredCountries, setFiltered] = useState([])
-  let responseData;
 
   const handleFilterChange = (value) => {
     setFilter(value);
@@ -24,21 +23,23 @@ const App = () => {
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        console.log('promise fulfilled')
-        responseData = response.data
-        console.log('response.data', responseData)
-        setCountries(responseData)
-        console.log("countries array", countries);
-        populateCountryNames();
+        console.log("promise fulfilled, response data:", response.data)
+        setCountries(response.data)
       })
+      .catch(error => console.log("Error from useEffect catch:", error))
   }, [])
+
+  useEffect(() => {
+    console.log("useEffect running because countries state changed");
+    populateCountryNames();
+  },[countries])
 
   function populateCountryNames() {
     const countryNames = countries.map(country =>
       country.name.common
     )
-    setNames(countryNames)
-    setFiltered(countryNames)
+    setNames(countryNames);
+    setFiltered(countryNames);
   }
 
   return (
@@ -81,13 +82,13 @@ const Information = ({filteredCountries, countries}) => {
     return (
     <div>
       <h2>{filteredCountries[0]} {countryInfo[0].flag}</h2>
-      <p>We've got lots of info on {filteredCountries[0]}!</p>
-      {/*
-      */}
       <img src={countryInfo[0].flags.png} alt="flag image" width="250"/>
       <p>Official Name: {countryInfo[0].name.official}</p>
-      <p>Region: {countryInfo[0].subregion}</p>
-      <p>Population: {countryInfo[0].population}</p>
+      <p>Languages: <ul>{Object.values(countryInfo[0].languages).map(lang =>
+          <li>{lang}</li>
+        )}</ul></p>
+      <p>Region: {countryInfo[0].subregion || countryInfo[0].region}</p>
+      <p>Population: {countryInfo[0].population.toLocaleString('en-US')}</p>
       <p>Coat of Arms:</p>
       <img src={countryInfo[0].coatOfArms.png} alt="coat of arms" width="250"/>
       <p>Capital City: {countryInfo[0].capital[0]}</p>
