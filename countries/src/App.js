0fbc305de445bf5728f73bb2066a60c79f9d8a5e
Joel Moran <joelbmoran@gmail.com>
2022-04-22 16:32:23 +0700

    Add countries weather info by calling API (exercise 2.14)

--- countries/src/App.js
+++ countries/src/App.js
@@ -94,7 +94,8 @@ const Information = ({filteredCountries, countries, wrapperSetFiltered}) => {
     <ViewOneCountry 
       countries={countries}
       filteredCountry={filteredCountries[0]}
-    />)
+    />
+    )
   } else if (length > 10) {
     return (
       <div>
@@ -114,29 +115,67 @@ const Information = ({filteredCountries, countries, wrapperSetFiltered}) => {
 }
 
 const ViewOneCountry = ({countries, filteredCountry}) => {
+  const [cityWeather, setCityWeather] = useState([])
+  const [weatherImg, setWeatherImg] = useState('')
+
   console.log("full info on one country", countries)
   let countryInfo = countries.filter(country =>
    country.name.common === filteredCountry
    )
   console.log("country info", countryInfo);
+
+  // call weather API
+  const weatherKey = process.env.REACT_APP_API_KEY;
+  useEffect(() => {
+    console.log('calling weather API')
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${countryInfo[0].latlng[0]}&lon=${countryInfo[0].latlng[1]}&appid=${weatherKey}&units=imperial`)
+      .then(response => {
+        console.log("promise fulfilled, response data:", response.data)
+        setCityWeather(response.data)
+      })
+      .catch(error => console.log("Error from useEffect catch:", error))
+  }, [])  
+
+  useEffect(() => {
+    console.log("useEffect running because cityWeather state changed", cityWeather);
+    if (Object.keys(cityWeather).length > 2) {
+      setWeatherImg("http://openweathermap.org/img/wn/" + cityWeather.weather[0].icon + "@2x.png")
+    }
+  },[cityWeather])
+
   return (
     <div>
-      <h2>{filteredCountry} {countryInfo[0].flag}</h2>
-      <img src={countryInfo[0].flags.png} alt="flag image" width="250"/>
-      <p>Official Name: {countryInfo[0].name.official}</p>
-      <p>Capital City: {countryInfo[0].capital[0]}</p>
-      <div><p>Languages: </p><ul>{Object.values(countryInfo[0].languages).map(lang =>
-          <li>{lang}</li>
-        )}</ul></div>
-      <p>Region: {countryInfo[0].subregion || countryInfo[0].region}</p>
-      <p>Population: {countryInfo[0].population.toLocaleString('en-US')}</p>
-      <p>{filteredCountry} is {countryInfo[0].independent ? "independent" : "not independent"
-      } and {countryInfo[0].landlocked ? "is landlocked" : "is not landlocked"
-      }. It {countryInfo[0].unMember ? "is" : "is not"} a member of the United Nations.
-      </p>
-      <p>Coat of Arms:</p>
-      <img src={countryInfo[0].coatOfArms.png} alt="coat of arms" width="250"/>
+      <div className="countryDiv">
+        <h2>{filteredCountry} {countryInfo[0].flag}</h2>
+        <img src={countryInfo[0].flags.png} alt="flag image" width="250"/>
+        <p>Official Name: {countryInfo[0].name.official}</p>
+        <p>Capital City: {countryInfo[0].capital[0]}</p>
+        <div><p>Languages: </p><ul>{Object.values(countryInfo[0].languages).map(lang =>
+            <li key={lang}>{lang}</li>
+          )}</ul></div>
+        <p>Region: {countryInfo[0].subregion || countryInfo[0].region}</p>
+        <p>Population: {countryInfo[0].population.toLocaleString('en-US')}</p>
+        <p>{filteredCountry} is {countryInfo[0].independent ? "independent" : "not independent"
+        } and {countryInfo[0].landlocked ? "is landlocked" : "is not landlocked"
+        }. It {countryInfo[0].unMember ? "is" : "is not"} a member of the United Nations.
+        </p>
+        <p>Coat of Arms:</p>
+        <img src={countryInfo[0].coatOfArms.png} alt="coat of arms" width="250"/>
       </div>
+      <div className="weatherDiv">
+        <h2>Current Weather in {countryInfo[0].capital[0]}</h2>
+        {Object.keys(cityWeather).length > 2 &&
+        <div>
+          <img alt="weather icon" src={weatherImg} />
+          <p>Weather: {cityWeather.weather[0].description}</p>
+          <p>Temperature: {cityWeather.main.temp}°F</p>
+          <p>Humidity: {cityWeather.main.humidity}%</p>
+          <p>Wind: {cityWeather.wind.speed} miles/hour</p>
+        </div>
+        }
+      </div>
+    </div>
       )
 }
 
